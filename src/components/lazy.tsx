1291cb855cbad31f671840c16219f6908f29415b
rankangan <rankangan@wps.cn>
2023-08-02 10:43:02 +0800

    feat: 修改子组件写法

--- src/components/lazy.tsx
+++ src/components/lazy.tsx
@@ -15,7 +15,7 @@ function Loading() {
 }
 
 // 懒加载
-export default function lazyLoad(Comp: React.LazyExoticComponent<any>): React.ReactNode {
+export function lazyLoad(Comp: React.LazyExoticComponent<any>): React.ReactNode {
   return (
     <Suspense fallback={<Loading />}>
       <Comp />
@@ -23,4 +23,13 @@ export default function lazyLoad(Comp: React.LazyExoticComponent<any>): React.Re
   )
 }
 
-lazyLoad.prototype.Loading = Loading
+// 该方式无法实现 lazyLoad.Loading 方式直接使用 Loading 组件
+// lazyLoad.prototype.Loading = Loading
+
+const lazy = lazyLoad as typeof lazyLoad & {
+  Loading: typeof Loading
+}
+
+lazy.Loading = Loading
+
+export default lazy
